6bde8737df7796a092a9fc726854d6cc081a79df
nishanpradhan <nishanpradhan500@gmail.com>
2025-05-01 21:30:28 +0545

    image constant

---
+++ lib/core/constant/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static const String logo = 'assets/logo/logo.png';
+  static const String icon = 'assets/logo/icon.png';
+  static const String placeholder = 'assets/images/placeholder.png';
+  static const String profileDefault = 'assets/images/profile_default.png';
+  static const String background = 'assets/images/bg_main.jpg';
+}
