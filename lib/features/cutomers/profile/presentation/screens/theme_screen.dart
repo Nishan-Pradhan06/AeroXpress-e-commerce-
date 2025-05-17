1ed6b3359fa4fa21a7563559eb24961bf624da4e
nishan <nishanpradhan500@gmail.com>
2025-05-17 21:05:13 +0545

    theme screen

--- lib/features/cutomers/profile/presentation/screens/theme_screen.dart
+++ lib/features/cutomers/profile/presentation/screens/theme_screen.dart
@@ -10,7 +10,24 @@ class ThemeSelectorScreen extends StatelessWidget {
     final selected = provider.themeMode;
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Choose Theme')),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: Material(
+          elevation: 0.1,
+          color: Colors.white,
+          child: AppBar(
+            iconTheme: const IconThemeData(color: Colors.black),
+            backgroundColor: Colors.white,
+            elevation: 0, // Set to 0 since Material provides elevation
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Theme", style: TextStyle(color: Colors.black)),
+              ],
+            ),
+          ),
+        ),
+      ),
       body: Column(
         children: [
           RadioListTile<ThemeMode>(
